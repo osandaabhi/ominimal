26eb1ff7eb5718ef87e293a69a33582952129398
osandaabhi <osandaabhi@gmail.com>
2026-02-04 11:40:44 -0500

    Adding definitions of DefinableFamily and IsStructure

--- boolean/Boolean/Basic.lean
+++ boolean/Boolean/Basic.lean
@@ -5,8 +5,26 @@ open BooleanAlgebra
 
 def seq (n : ℕ) := BooleanAlgebra (Set (Fin n →  ℝ) )
 
+-- #print BooleanAlgebra
 
-#print BooleanAlgebra
+variable (s t : Set ℝ)
 
-example s ⊓ sᶜ = (⊥ : Set ℝ) := by
-  exact inf_compl_eq_bot
+-- #check (inferInstance : BooleanAlgebra (Set ℝ))
+
+#check inf_compl_eq_bot
+
+example : s ⊓ sᶜ = (⊥ : Set ℝ) := by simpa using (inf_compl_eq_bot s)
+
+universe u
+
+structure DefinableFamily :=
+  (definable : ∀ n: ℕ, Set (Set (Fin n → ℝ)))
+
+def IsStrucure (S : DefinableFamily) : Prop :=
+  ∀ n : ℕ, /-
+    (∀ A : Set (Fin n → ℝ), A ∈ S.definable n →
+      (∀ i : Fin n, ∃ U : Set ℝ, U ∈ S.definable 1 ∧
+        (∀ x : Fin n → ℝ, x ∈ A → x i ∈ U))) ∧-/
+    (∀ U V : Set (Fin n →  ℝ), U ∈ S.definable n → V ∈ S.definable n → (U ∩ V) ∈ S.definable n) ∧
+    (∀ U : Set (Fin n → ℝ), U ∈ S.definable n → ((Set (Fin n → ℝ) \ U) ∈ S.definable n))
+  sorry
